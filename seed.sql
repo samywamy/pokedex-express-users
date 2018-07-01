5adf70b6ecbd86c7f89b50273cf2474c3450b9ef
samywamy <sam.alexis.ng@gmail.com>
2018-07-02 00:14:08 +0800

    further 1

---
+++ seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO types 
+(type)
+VALUES
+('grass'), ('electric'), ('ground'), ('ice'), ('water'), ('fire'), ('normal');
+
+
+INSERT INTO pokemon_types
+(pokemon_id, type)
+VALUES
+(1, 'grass'), (1, 'ground'), (7, 'water'), (4, 'fire'), (10, 'grass'), (10, 'ground'), (25, 'electric');
+
--- tables.sql
+++ tables.sql
@@ -1,21 +1,32 @@
 DROP TABLE pokemon;
 
 CREATE TABLE IF NOT EXISTS pokemon (
-  id SERIAL PRIMARY KEY,
-  num varchar(255),
-  name varchar(255),
-  img varchar(255),
-  weight varchar(255),
-  height varchar(255)
+	id SERIAL PRIMARY KEY,
+	num varchar(255),
+	name varchar(255),
+	img varchar(255),
+	weight varchar(255),
+	height varchar(255)
 );
 
 ALTER TABLE pokemon ADD user_id Integer; 
 
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  password_hash varchar(255),
-  email varchar(255)
+	id SERIAL PRIMARY KEY,
+	password_hash varchar(255),
+	email varchar(255)
 );
 
-ALTER TABLE pokemon
-ADD FOREIGN KEY (user_id) REFERENCES users(id); 
+ALTER TABLE pokemon ADD FOREIGN KEY (user_id) REFERENCES users(id); 
+
+CREATE TABLE IF NOT EXISTS types (
+	type varchar(255) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS pokemon_types (
+	pokemon_id integer,
+	type varchar(255)
+);
+
+ALTER TABLE pokemon_types ADD FOREIGN KEY (pokemon_id) REFERENCES pokemon(id); 
+ALTER TABLE pokemon_types ADD FOREIGN KEY (type) REFERENCES types(type); 
